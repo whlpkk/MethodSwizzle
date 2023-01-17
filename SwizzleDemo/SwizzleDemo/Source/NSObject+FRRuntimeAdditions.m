714bd4d3bd4694a8a8d47fac01b25127267ffa36
yaozhikun <yaozhikun.whlpkk1@bytedance.com>
2023-01-17 11:24:39 +0800

    修改

--- SwizzleDemo/SwizzleDemo/Source/NSObject+FRRuntimeAdditions.m
+++ SwizzleDemo/SwizzleDemo/Source/NSObject+FRRuntimeAdditions.m
@@ -11,7 +11,18 @@ void class_swizzleInstanceMethod(Class class, SEL originalSEL, SEL replacementSE
 {
     //class_getInstanceMethod()，如果子类没有实现相应的方法，则会返回父类的方法。
     Method originMethod = class_getInstanceMethod(class, originalSEL);
+    NSCAssert(NULL != originMethod,
+              @"Selector %@ not found in %@ methods of class %@.",
+              NSStringFromSelector(originalSEL),
+              class_isMetaClass(class) ? @"class" : @"instance",
+              class);
+    
     Method replaceMethod = class_getInstanceMethod(class, replacementSEL);
+    NSCAssert(NULL != replaceMethod,
+              @"Selector %@ not found in %@ methods of class %@.",
+              NSStringFromSelector(replacementSEL),
+              class_isMetaClass(class) ? @"class" : @"instance",
+              class);
     
     //class_addMethod() 判断originalSEL是否在子类中实现，如果只是继承了父类的方法，没有重写，那么直接调用method_exchangeImplementations，则会交换父类中的方法和当前的实现方法。此时如果用父类调用originalSEL，因为方法已经与子类中调换，所以父类中找不到相应的实现，会抛出异常unrecognized selector.
     //当class_addMethod() 返回YES时，说明子类未实现此方法(根据SEL判断)，此时class_addMethod会添加（名字为originalSEL，实现为replaceMethod）的方法。此时在将replacementSEL的实现替换为originMethod的实现即可。
